4d9f091c243ed5fb839a67372c95ae182217d262
Vili Lipo <vili.lipo@helsinki.fi>
2018-09-28 21:22:26 +0300

    Set sensible map size

--- snake-ai/src/main/java/fi/tiralabra/game/GameArea.java
+++ snake-ai/src/main/java/fi/tiralabra/game/GameArea.java
@@ -16,8 +16,8 @@ package fi.tiralabra.game;
 public class GameArea implements Cloneable {
 
     int[][] table;
-    private final int width = 100;
-    private final int height = 100;
+    private final int width = 25;
+    private final int height = 25;
     private final static int FREE = 0;
     private final static int SNAKE = 1;
     private final static int APPLE = 2;
